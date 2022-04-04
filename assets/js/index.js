a68312bd1675772dba07ee9896a5a2434d6701d3
jenadiusnicholaus <ucode888@gmail.com>
2022-04-04 13:40:38 +0300

    add service card

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,27 @@
 document.getElementById('home').addEventListener('click', (event) => {
     document.getElementById('home-content').style.display='block';
-    document.getElementById('about-us-content').style.display='none';
 });
 
 document.getElementById('about').addEventListener('click', (event) => {
     document.getElementById('home-content').style.display='none';
     document.getElementById('about-us-content').style.display='block';
     document.getElementById('contact-us-content').style.display='none';
+    document.getElementById('service-content').style.display='none';
+
 
 });
 
 document.getElementById('contact').addEventListener('click', (event) => {
     document.getElementById('contact-us-content').style.display='block';
+    document.getElementById('home-content').style.display='none';
+    document.getElementById('about-us-content').style.display='none';
+    document.getElementById('service-content').style.display='none';
+
+});
+
+document.getElementById('service').addEventListener('click', (event) => {
+    document.getElementById('service-content').style.display='block';
+    document.getElementById('contact-us-content').style.display='none';
     document.getElementById('home-content').style.display='none';
     document.getElementById('about-us-content').style.display='none';
 });
